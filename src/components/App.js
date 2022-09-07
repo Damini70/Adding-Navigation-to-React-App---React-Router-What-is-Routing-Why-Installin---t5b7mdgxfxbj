b29de65752ed523af5f64bb9ec5e67e0598dc242
Damini Raj <daminiraj70@gmail.com>
2022-09-07 09:44:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, {Component, useState} from "react";
+import {BrowserRouter as Router,Link,Route,Routes} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import '../styles/App.css';
 
 class App extends Component {
@@ -6,7 +8,17 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <Router>
+        <Link to="/">Home</Link><br/>
+        <Link to="/about">About</Link><br/>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/*" element={<div><h1>No match.</h1></div>}/>
+        </Routes>
+        <LocationDisplay/>
+
+        </Router>
             </div>
         )
     }
@@ -14,3 +26,36 @@ class App extends Component {
 
 
 export default App;
+
+
+class Home extends Component {
+    render() {
+
+        return(
+            <div>
+                <h1>You are home.</h1>
+            </div>
+        )
+    }
+}
+
+class About extends Component {
+    render() {
+
+        return(
+            <div>
+                 <h1>You are on the about page.</h1>
+            </div>
+        )
+    }
+}
+function LocationDisplay(){
+    const location = useLocation();
+    // console.log(location)
+
+    return (
+        <div>
+        <h1 data-testid="location-display">{location.pathname}</h1>
+        </div >
+    )
+}

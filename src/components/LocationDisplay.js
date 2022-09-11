0fc58572d11092204a84e8b1aadedcf3f43f0b75
Damini Raj <daminiraj70@gmail.com>
2022-09-11 20:17:50 +0530

    Update LocationDisplay.js

--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-export default function LocationDisplay(){
-    const location = useLocation();
-    // console.log(location)
 
-    return (
-        <div>
-        <h1 data-testid="location-display">{location.pathname}</h1>
-        </div >
-    )
+export default function LocationDisplayComp() {
+  let location = useLocation();
+  return <div data-testid="location-display">{location.pathname}</div>;
 }
